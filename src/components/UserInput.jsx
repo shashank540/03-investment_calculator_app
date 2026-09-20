c19cdd0d3a5ae2735213a8c27cfeb6f6d764f722
shashank540 <shashank.srivastava5@gmail.com>
2026-09-20 22:45:09 +0530

    feat: Changes to calculate investment in Indian currency

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -6,14 +6,14 @@ export default function UserInput({onChange, userInput}) {
     <section id="user-input">
         <div className="input-group">
           <p>
-            <label>Initial Investment ($)</label>
+            <label>Initial Investment (₹)</label>
             <input type="number" required value={userInput.initialInvestment} onChange={(event) => 
             onChange('initialInvestment', event.target.value)
         }
              />
           </p>
           <p>
-            <label>Annual Investment ($)</label>
+            <label>Annual Investment (₹)</label>
             <input type="number" required value={userInput.annualInvestment} 
             onChange={(event) => onChange('annualInvestment', event.target.value)} />
           </p>
--- src/util/investment.js
+++ src/util/investment.js
@@ -29,10 +29,12 @@ export function calculateInvestmentResults({
 
 // The browser-provided Intl API is used to prepare a formatter object
 // This object offers a "format()" method that can be used to format numbers as currency
-// Example Usage: formatter.format(1000) => yields "$1,000"
-export const formatter = new Intl.NumberFormat('en-US', {
+// Example Usage: formatter.format(150000) => yields "₹1.5L"
+export const formatter = new Intl.NumberFormat('en-IN', {
   style: 'currency',
-  currency: 'USD',
+  currency: 'INR',
+  notation: 'compact',
+  compactDisplay: 'short',
   minimumFractionDigits: 0,
-  maximumFractionDigits: 0,
+  maximumFractionDigits: 1,
 });
